8d41a54b76ced6d72588893e05de2071ebdd88e5
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-04-13 13:16:20 +1000

    theme changed

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -131,9 +131,9 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 # html_theme = 'bizstyle'
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 # html_theme = 'classic'
 # html_theme = 'nature'
 
