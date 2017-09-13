ea79d6dc4daaac06fcad721ec066d97400bf942b
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-09-13 13:18:32 +1000

    version number increased

--- nutsflow/__init__.py
+++ nutsflow/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.24'
+__version__ = '1.0.25'
 
 from nutsflow.source import Enumerate, Repeat, Product, Empty, Range, ReadCSV
 from nutsflow.processor import (Take, Slice, Concat, Interleave, Zip, ZipWith,
